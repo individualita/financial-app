420b708772bb04e1a381e0e4a71545b68e6d89b1
Roman <asdwka3@gmail.com>
2024-10-12 15:27:45 +0200

    refactor: renove unused button component

--- src/components/pages/welcome/Welcome.jsx
+++ src/components/pages/welcome/Welcome.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 
-import Button from '../../common/buttons/Button';
 import welcomeImg from '../../../assets/images/welcome/girlwithphone.png'
 import Heading from '../../common/headings/Heading';
 
@@ -14,12 +13,8 @@ const Welcome = () => {
         <section className={styles.welcome} aria-labelledby="welcome-title">
             <img className={styles.img} src={welcomeImg} role="img" alt="A girl sits on a coin and looks at her phone." />
             <Heading level={1} id="welcome-title">Take Control of Your Finances </Heading>
-            <Button
-                className="button button-primary button-large"
-                aria-label="Click to start navigating to the Dashboard"
-                >
-                    <Link className={styles.startLink}  to="/home" aria-label="Go to home">Click to start </Link>
-            </Button>
+
+            <Link className={styles.startLink}  to="/home" aria-label="Go to home">Click to start </Link>
             
             
             <footer className={styles.footer} role="contentinfo">
