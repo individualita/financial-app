44752f751c9d055f82fc5aa72db63c8a8d069fdb
Roman <asdwka3@gmail.com>
2024-10-27 18:21:38 +0100

    feat: back to home page link in Notfoundpage component

--- src/components/pages/notfoundpage/Notfoundpage.jsx
+++ src/components/pages/notfoundpage/Notfoundpage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import dogImg from './dog.png';
 import styles from './notfoundpage.module.scss';
 
@@ -6,7 +7,8 @@ const Notfoundpage = () => {
         <section className={styles.error}>
             <div className={styles.type}>404</div>
             <p className={styles.message}>A Dog Ate this Page</p>
-            <img className={styles.img}  src={dogImg} alt="" />
+            <img className={styles.img}  src={dogImg} alt="Brown dog with a piece of paper in its mouth." />
+            <Link to="/" style={{padding: '8px', borderBottom: '1px solid gray'}}>Back to main page</Link>
         </section>
     )
 }
